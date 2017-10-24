36a589e793a14cb5d211d8b40d52316ffcfe2de7
Jinzhou Zhang <lotabout@gmail.com>
2017-10-24 23:00:50 +0800

    tmp commit

--- src/me/lotabout/codegenerator/action/CodeGeneratorAction.java
+++ src/me/lotabout/codegenerator/action/CodeGeneratorAction.java
@@ -1,13 +1,36 @@
 package me.lotabout.codegenerator.action;
 
-import com.intellij.codeInsight.generation.actions.BaseGenerateAction;
+import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.actions.CodeInsightAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
-public class CodeGeneratorAction extends BaseGenerateAction {
+public class CodeGeneratorAction extends CodeInsightAction {
     private String templateKey;
+    private final CodeInsightActionHandler myHandler;
 
     public CodeGeneratorAction(String templateKey, String templateName) {
-        super(new CodeGeneratorActionHandler(templateKey));
         getTemplatePresentation().setDescription("description");
         getTemplatePresentation().setText(templateName, false);
+        this.myHandler = new CodeGeneratorActionHandler(templateKey);
+    }
+
+    @NotNull
+    @Override
+    protected CodeInsightActionHandler getHandler() {
+        return myHandler;
+    }
+
+
+    @Override
+    public void update(AnActionEvent e) {
+        // Code Generation action could run without editor
+        Presentation presentation = e.getPresentation();
+        Project project = e.getProject();
+        if (project == null) {
+            presentation.setEnabled(false);
+        }
     }
 }
